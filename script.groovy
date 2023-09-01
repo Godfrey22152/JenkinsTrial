fce27835d5b3e77c14ec40f671bd1bc2a13fc6c1
Godfrey22152 <90422561+Godfrey22152@users.noreply.github.com>
2023-09-01 17:11:14 +0100

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+    echo 'Building the Application'
+}
+
+def testApp() {
+    echo 'Testing the Application'
+}
+
+def deployApp() {
+    echo 'Deployimg the Application'
+}
+
+return this
